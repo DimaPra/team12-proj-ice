b6b91be523aa9569220ce0551111709dff893865
DimaPra <digffffffu@gmail.com>
2022-09-06 22:08:07 +0300

    fix js

--- src/modal-location.js
+++ src/modal-location.js
@@ -2,7 +2,7 @@
 	const refs = {
 		openModalBtn: document.querySelector("[data-btn2-open]"),
 		closeModalBtn: document.querySelector("[data-btn2-close]"),
-		modal: document.querySelector("[data-modal2-franchise]"),
+		modal: document.querySelector("[data-modal-location]"),
 	};
 
 	refs.openModalBtn.addEventListener("click", toggleModal);
